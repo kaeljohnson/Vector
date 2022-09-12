019dd86506d08e4254b168f3e5c923d8a19fef88
Kael Johnson <kaeljohnson6@gmail.com>
2022-09-12 09:14:45 -0600

    Can't compile templated functions in separate CPP class

--- Vector.h
+++ Vector.h
@@ -12,20 +12,66 @@ class Vector {
     std::array<type, dimension> point;
 
   public:
-    Vector(std::array<type, dimension> arr);
+    Vector(std::array<type, dimension> arr) {
+      point = arr;
+    }
 
-    type dot(Vector<type, dimension> &toDot);
-    type cross2D(Vector<type, 2> &toCross);
-    Vector<type, 3> cross3D(Vector<type, 3> &toCross);
+    type dot(Vector<type, dimension> &toDot) {
+      type dotted = 0;
+      for (int idx = 0; idx < dimension; idx++) {
+        dotted += point[idx] * toDot[idx];
+      }
+      return dotted;
+    }
 
-    Vector<type, dimension> scale(Vector<type, dimension> &toScale);
+    type cross2D(Vector<type, 2> &toCross) {
+      return point[0] * toCross[1] - point[1] * toCross[0];
+    }
 
-    Vector<type, dimension> operator+(Vector<type, dimension> &toAdd);
-    Vector<type, dimension> operator-(Vector<type, dimension> &toSub);
-    Vector<type, dimension> operator*(int toScale);
-    type &operator[](size_t idx);
+    Vector<type, 3> cross3D(Vector<type, 3> &toCross) {
+      std::array<type, dimension> crossProduct;
+      crossProduct[0] = point[1] * toCross[2] - point[2] * toCross[1]; 
+      crossProduct[1] = point[2] * toCross[0] - point[0] * toCross[2];
+      crossProduct[2] = point[0] * toCross[1] - point[1] * toCross[0];
 
-    std::array<type, dimension> getPoint();
+      return crossProduct;
+    }
+
+    Vector<type, dimension> operator+(Vector<type, dimension> &toAdd) {
+      std::array<type, dimension> newPoint;
+      for (size_t idx = 0; idx < dimension; idx++) {
+        newPoint[idx] = point[idx] + toAdd[idx];
+      } 
+
+      Vector<type, dimension> addedVec (newPoint);
+      return addedVec;
+    }
+
+    Vector<type, dimension> operator-(Vector<type, dimension> &toSub) {
+      std::array<type, dimension> newPoint;
+      for (size_t idx = 0; idx < dimension; idx++) {
+        newPoint[idx] = point[idx] - toSub[idx];
+      } 
+      Vector<type, dimension> subtractedVec (newPoint);
+      return subtractedVec;
+    }
+
+    Vector<type, dimension> operator*(int toScale) {
+      std::array<type, dimension> newPoint;
+      for (size_t idx = 0; idx < dimension; idx++) {
+        newPoint[idx] = point[idx] * toScale;
+      } 
+      Vector<type, dimension> multipliedVec (newPoint);
+      return multipliedVec;
+    }
+
+    type &operator[](size_t idx) {
+      return point[idx];
+    }
+
+    std::array<type, dimension> getPoint() {
+      return point;
+    }
 };
 
 #endif
--- test.cpp
+++ test.cpp
@@ -7,54 +7,71 @@ int main() {
     std::array<int, 2> arrTwoD = {1, 2};
     Vector<int, 2> twoD (arrTwoD);
 
-    //std::array<int, 3> arrThreeD = {1, 2, 3};
-    //Vector<int, 3> threeD (arrThreeD);
+    std::array<int, 3> arrThreeD = {1, 2, 3};
+    Vector<int, 3> threeD (arrThreeD);
 
-    //std::array<int, 3> arrThreeD2 = {3, 4, 5};
-    //Vector<int, 3> threeD2 (arrThreeD2);
+    std::array<int, 3> arrThreeD2 = {3, 4, 5};
+    Vector<int, 3> threeD2 (arrThreeD2);
 
-    //std::array<double, 2> arrDoubleTwoD = {1.1, 2.2};
-    //Vector<double, 2> doubleTwoD (arrDoubleTwoD);
+    std::array<double, 2> arrDoubleTwoD = {1.1, 2.2};
+    Vector<double, 2> doubleTwoD (arrDoubleTwoD);
 
-    //std::array<double, 3> arrDoubleThreeD = {1.1, 2.2, 3.3};
-    //Vector<double, 3> doubleThreeD (arrDoubleThreeD);
+    std::array<double, 3> arrDoubleThreeD = {1.1, 2.2, 3.3};
+    Vector<double, 3> doubleThreeD (arrDoubleThreeD);
     
-    //std::array<int, 2> getTwoD = twoD.getPoint();
-    //for (auto i : getTwoD) {
-    //    std::cout << i << std::endl;
-    //}
+    std::array<int, 2> getTwoD = twoD.getPoint();
 
-    //int dotProdTwoD = twoD.dot(twoD);
-    //std::cout << dotProdTwoD << std::endl;
+    std::cout << "Two D Vector Points\n";
+    for (auto i : getTwoD) {
+        std::cout << i << ", ";
+    }
+    std::cout << std::endl;
 
-    //int crossProd2D = twoD.cross2D(twoD);
-    //std::cout << crossProd2D << std::endl;
+    int dotProdTwoD = twoD.dot(twoD);
 
-    //Vector<int, 3> crossProd3D = threeD.cross3D(threeD2);
-    //std::array<int, 3> getCrossProd3D = crossProd3D.getPoint();
-    //for (auto i : getCrossProd3D) {
-    //    std::cout << i << std::endl;
-    //}
+    std::cout << "Two D Vector Dot Product\n";
+    std::cout << dotProdTwoD << std::endl;
 
-    //Vector<int, 2> newAddTwoD = twoD + twoD;
-    //std::array<int, 2> getNewAddTwoD = newAddTwoD.getPoint();
-    //for (auto i : getNewTwoD) {
-    //    std::cout << i << std::endl;
-    //}
+    int crossProd2D = twoD.cross2D(twoD);
 
-    //Vector<int, 2> newSubTwoD = twoD - twoD;
-    //std::array<int, 2> getNewSubTwoD = newSubTwoD.getPoint();
-    //for (auto i : getNewSubTwoD) {
-    //    std::cout << i << std::endl;
-    //}
+    std::cout << "Two D Vector Cross Product\n";
+    std::cout << crossProd2D << std::endl;
 
-    //Vector<int, 2> newMultTwoD = twoD * 2.0;
-    //std::array<int, 2> getNewMultTwoD = newMultTwoD.getPoint();
-    //for (auto i : getNewMultTwoD) {
-    //    std::cout << i << std::endl;
-    //}
+    Vector<int, 3> crossProd3D = threeD.cross3D(threeD2);
+    std::array<int, 3> getCrossProd3D = crossProd3D.getPoint();
 
-    //std::cout << twoD[0] << endl;
+    std::cout << "Three D Vector Cross Product\n";
+    for (auto i : getCrossProd3D) {
+        std::cout << i << ", ";
+    }
+    std::cout << std::endl;
+
+    Vector<int, 2> newAddTwoD = twoD + twoD;
+    std::array<int, 2> getNewAddTwoD = newAddTwoD.getPoint();
+
+    std::cout << "Add Two D Vectors\n";
+    for (auto i : getNewAddTwoD) {
+        std::cout << i << ", ";
+    }
+    std::cout << std::endl;
+
+    Vector<int, 2> newSubTwoD = twoD - twoD;
+    std::array<int, 2> getNewSubTwoD = newSubTwoD.getPoint();
+
+    std::cout << "Subtract Two D Vectors\n";
+    for (auto i : getNewSubTwoD) {
+        std::cout << i << ", ";
+    }
+    std::cout << std::endl;
+
+    Vector<int, 2> newMultTwoD = twoD * 2.0;
+    std::array<int, 2> getNewMultTwoD = newMultTwoD.getPoint();
+
+    std::cout << "Two D Vector multiplied by 2\n";
+    for (auto i : getNewMultTwoD) {
+        std::cout << i << ", ";
+    }
+    std::cout << std::endl;
     
     return 0;
 }
